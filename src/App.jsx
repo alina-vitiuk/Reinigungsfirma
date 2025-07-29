1c52763e8e374389f38be2c8a510fc12b58da42e
alina-vitiuk <vityukalina@gmail.com>
2025-07-29 12:10:25 +0200

    added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <AppRoutes />
       <Footer />
